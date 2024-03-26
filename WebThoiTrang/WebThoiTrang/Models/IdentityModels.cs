d4d3d00c31e1954dcb7f8f117a3d8456c814bcbd
congvuhuy <cong19112002@gmail.com>
2024-03-26 23:54:25 +0700

    Tao bang CSDL

--- WebThoiTrang/WebThoiTrang/Models/IdentityModels.cs
+++ WebThoiTrang/WebThoiTrang/Models/IdentityModels.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using WebThoiTrang.Models.EF;
 
 namespace WebThoiTrang.Models
 {
@@ -24,7 +25,19 @@ namespace WebThoiTrang.Models
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
-
+        public DbSet<ThongKe> ThongKes { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Adv> Advs { get; set; }
+        public DbSet<Posts> Posts { get; set; }
+        public DbSet<News> News { get; set; }
+        public DbSet<SystemSetting> SystemSettings { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
